f31e5778a41f740d97324433d5b08dec7bac8110
mbzos45 <90610406+mbzos45@users.noreply.github.com>
2023-09-26 09:50:52 +0900

    課題1 追加実装

--- udp/dg_client.c
+++ udp/dg_client.c
@@ -1,27 +1,27 @@
 /* コネクションレスの簡単なデータ検索クライアント(dg_client.c) */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h> /* ソケットのための基本的なヘッダファイル      */
 #include <netinet/in.h> /* インタネットドメインのためのヘッダファイル  */
 #include <netdb.h>      /* gethostbyname()を用いるためのヘッダファイル */
-#include <errno.h>
 #include <string.h>
-#define  MAXHOSTNAME    64
+#include <unistd.h>
+
+#define  MAX_HOSTNAME    64
 #define     S_UDP_PORT    (u_short)5000
-#define  MAXKEYLEN    128
-#define     MAXDATALEN    256
+#define  MAX_KEY_LEN    128
+#define     MAX_DATA_LEN    256
 
-int setup_dgclient(struct hostent *, u_short, struct sockaddr_in *, int *);
+int setup_dgclient(struct hostent *, u_short, struct sockaddr_in *, socklen_t *);
 
-void remote_dbsearch(int, struct sockaddr_in *, int);
+void remote_dbsearch(int, struct sockaddr_in *, socklen_t);
 
-main() {
+int main() {
     int socd;
-    char s_hostname[MAXHOSTNAME];
+    char s_hostname[MAX_HOSTNAME];
     struct hostent *s_hostent;
     struct sockaddr_in s_address;
-    int s_addrlen;
+    socklen_t s_addrlen;
 
     /* サーバのホスト名の入力 */
     printf("server host name?: ");
@@ -42,7 +42,7 @@ main() {
     exit(0);
 }
 
-int setup_dgclient(struct hostent *hostent, u_short port, struct sockaddr_in *s_addressp, int *s_addrlenp) {
+int setup_dgclient(struct hostent *hostent, u_short port, struct sockaddr_in *s_addressp, socklen_t *s_addrlenp) {
     int socd;
     struct sockaddr_in c_address;
 
@@ -51,7 +51,6 @@ int setup_dgclient(struct hostent *hostent, u_short port, struct sockaddr_in *s_
         perror("socket");
         exit(1);
     }
-
     /* サーバのアドレス(Internetアドレスとポート番号)の作成 */
     bzero((char *) s_addressp, sizeof(*s_addressp));
     s_addressp->sin_family = AF_INET;
@@ -74,27 +73,32 @@ int setup_dgclient(struct hostent *hostent, u_short port, struct sockaddr_in *s_
     return socd;
 }
 
-void remote_dbsearch(int socd, struct sockaddr_in *s_addressp, int s_addrlen) /* サーバにキーを送り検索結果(データ)を受け取る */
+void remote_dbsearch(int socd, struct sockaddr_in *s_addressp, socklen_t s_addrlen) /* サーバにキーを送り検索結果(データ)を受け取る */
 {
-    char key[MAXKEYLEN + 1], data[MAXDATALEN + 1];
-    int keylen, datalen;
-
-    /* キーを標準入力から入力 */
-    printf("key?: ");
-    scanf("%s", key);
-    /* キーをソケットに書き込む */
-    keylen = strlen(key);
-    if (sendto(socd, key, keylen, 0, (struct sockaddr *) s_addressp, s_addrlen) != keylen) {
-        fprintf(stderr, "datagram error\n");
-        exit(1);
-    }
-    /* 検索データをソケットから読み込む */
-    if ((datalen = recvfrom(socd, data, MAXDATALEN, 0, NULL, &s_addrlen)) < 0) {
-        perror("recvfrom");
-        exit(1);
-    }
-    /* データを標準出力に出力 */
-    data[datalen] = '\0';
-    fputs("data: ", stdout);
-    puts(data);
+    char key[MAX_KEY_LEN + 1], data[MAX_DATA_LEN + 1];
+    ssize_t datalen;
+    size_t keylen;
+    do {
+        /* キーを標準入力から入力 */
+        printf("key?: ");
+        if (scanf("%s", key) == EOF) {
+            return;
+        }
+        /* キーをソケットに書き込む */
+        keylen = strlen(key);
+        if (sendto(socd, key, keylen, 0, (struct sockaddr *) s_addressp, s_addrlen) != keylen) {
+            fprintf(stderr, "datagram error\n");
+            exit(1);
+        }
+        /* 検索データをソケットから読み込む */
+        if ((datalen = recvfrom(socd, data, MAX_DATA_LEN, 0, NULL, &s_addrlen)) < 0) {
+            perror("recvfrom");
+            exit(1);
+        }
+        /* データを標準出力に出力 */
+        data[datalen] = '\0';
+        fputs("data: ", stdout);
+        puts(data);
+        printf("\n");
+    } while (1);
 }
--- udp/dg_server.c
+++ udp/dg_server.c
@@ -1,93 +1,140 @@
 /* コネクションレスの簡単なデータ検索サーバ(dg_server.c) */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h> /* ソケットのための基本的なヘッダファイル      */
 #include <netinet/in.h> /* インタネットドメインのためのヘッダファイル  */
 #include <netdb.h>      /* gethostbyname()を用いるためのヘッダファイル */
-#include <errno.h>
 #include <string.h>
-#define  MAXHOSTNAME	64
-#define  S_UDP_PORT	(u_short)5000  /* 本サーバが用いるポート番号 */
-#define  MAXKEYLEN	128
-#define  MAXDATALEN	256
-int setup_dgserver(struct hostent*, u_short);
+#include <unistd.h>
+
+#define  MAX_HOSTNAME    64
+#define  S_UDP_PORT    (u_short)5000  /* 本サーバが用いるポート番号 */
+#define  MAX_KEY_LEN    128
+#define  MAX_DATA_LEN    256
+
+#define SPLIT_SYMBOL ","
+#define GET_HEADER "GET:"
+#define POST_HEADER "POST:"
+
+int setup_dgserver(struct hostent *, u_short);
+
 void db_search(int);
 
-main()
-{
-	int	socd;
-	char	s_hostname[MAXHOSTNAME];
-	struct hostent	*s_hostent;
+int main() {
+    int socd;
+    char s_hostname[MAX_HOSTNAME];
+    struct hostent *s_hostent;
 
-	/* サーバのホスト名とそのInternetアドレス(をメンバに持つhostent構造体)を求める */
-	gethostname(s_hostname, sizeof(s_hostname));
-	s_hostent = gethostbyname(s_hostname);
+    /* サーバのホスト名とそのInternetアドレス(をメンバに持つhostent構造体)を求める */
+    gethostname(s_hostname, sizeof(s_hostname));
+    s_hostent = gethostbyname(s_hostname);
 
-	/* データグラムサーバの初期設定 */
-	socd = setup_dgserver(s_hostent, S_UDP_PORT);
+    /* データグラムサーバの初期設定 */
+    socd = setup_dgserver(s_hostent, S_UDP_PORT);
 
-	/* クライアントからのデータ検索要求の処理 */
-	db_search(socd);
+    /* クライアントからのデータ検索要求の処理 */
+    db_search(socd);
 }
 
-int setup_dgserver(struct hostent *hostent, u_short port)
-{
-	int	socd;
-	struct sockaddr_in	s_address;
+int setup_dgserver(struct hostent *hostent, u_short port) {
+    int socd;
+    struct sockaddr_in s_address;
 
-	/* インターネットドメインのSOCK_DGRAM(UDP)型ソケットの構築 */
-	if((socd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { perror("socket");exit(1); }
+    /* インターネットドメインのSOCK_DGRAM(UDP)型ソケットの構築 */
+    if ((socd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
 
-	/* アドレス(Internetアドレスとポート番号)の作成 */
-	bzero((char *)&s_address, sizeof(s_address));
-	s_address.sin_family = AF_INET;
-	s_address.sin_port = htons(port);
-	bcopy((char *)hostent->h_addr, (char *)&s_address.sin_addr, hostent->h_length);
+    /* アドレス(Internetアドレスとポート番号)の作成 */
+    bzero((char *) &s_address, sizeof(s_address));
+    s_address.sin_family = AF_INET;
+    s_address.sin_port = htons(port);
+    bcopy((char *) hostent->h_addr, (char *) &s_address.sin_addr, hostent->h_length);
 
-	/* アドレスのソケットへの割り当て */
-	if(bind(socd, (struct sockaddr *)&s_address, sizeof(s_address)) < 0) { perror("bind");exit(1); }
+    /* アドレスのソケットへの割り当て */
+    if (bind(socd, (struct sockaddr *) &s_address, sizeof(s_address)) < 0) {
+        perror("bind");
+        exit(1);
+    }
 
-	return socd;
+    return socd;
 }
 
-void db_search(int socd) /* クライアントがデータ検索要求を処理する */
+void db_search(int socd) /* クライアントのデータ検索要求を処理する */
 {
-	struct sockaddr_in	c_address;
-	int	c_addrlen;
-	char	key[MAXKEYLEN+1], data[MAXDATALEN+1];
-	int	keylen, datalen;
-	static char *db[] = {"amano-taro","0426-91-9418","ishida-jiro","0426-91-9872",
-                             "ueda-saburo","0426-91-9265","ema-shiro","0426-91-7254",
-                             "ooishi-goro","0426-91-9618",NULL};
-	char	**dbp;
-
-	while(1) {
-		/* キーをソケットから読み込む */
-		c_addrlen = sizeof(c_address);
-		if((keylen = recvfrom(socd, key, MAXKEYLEN, 0, (struct sockaddr *)&c_address, &c_addrlen)) < 0) {
-			perror("recvfrom");
-			exit(1);
-		}
-		key[keylen] = '\0';
-		printf("Received key> %s\n",key);
-		/* キーを用いてデータ検索 */
-		dbp = db;
-		while(*dbp) {
-			if(strcmp(key, *dbp) == 0) {
-				strcpy(data, *(++dbp));
-				break;
-			}
-			dbp += 2;
-		}
-		if(*dbp == NULL) strcpy(data, "No entry");
-	
-		/* 検索したデータをソケットに書き込む */
-		datalen = strlen(data);
-		if(sendto(socd, data, datalen, 0, (struct sockaddr *)&c_address, c_addrlen) != datalen) {
-			fprintf(stderr, "datagram error\n"); 
-			exit(1);
-		}
-		printf("Sent data> %s\n", data);
-	}
+    struct sockaddr_in c_address;
+    int i;
+    char keys[MAX_KEY_LEN + 1] = {0}, data[MAX_DATA_LEN + 1] = {0}, removeHeaderKeys[
+            MAX_KEY_LEN + 1] = {0}, postTargetName[MAX_KEY_LEN] = {0};
+    const int GET_HEADER_LEN = strlen(GET_HEADER), POST_HEADER_LEN = strlen(POST_HEADER);
+    size_t keysLen, dataLen, headID = 0;
+    socklen_t c_addrlen;
+    static char *db[11] = {"amano-taro", "0426-91-9418", "ishida-jiro", "0426-91-9872",
+                           "ueda-saburo", "0426-91-9265", "ema-shiro", "0426-91-7254",
+                           "ooishi-goro", "0426-91-9618", NULL};
+    char **dbp;
+    while (1) {
+        /* キーをソケットから読み込む */
+        c_addrlen = sizeof(c_address);
+        if ((keysLen = recvfrom(socd, keys, MAX_KEY_LEN, 0, (struct sockaddr *) &c_address, &c_addrlen)) < 0) {
+            perror("recvfrom");
+            exit(1);
+        }
+        keys[keysLen] = '\0';
+        printf("Received keys> %s\n", keys);
+        if (strncmp(keys, GET_HEADER, GET_HEADER_LEN) == 0) {
+            strcat(removeHeaderKeys, &keys[GET_HEADER_LEN + 1]);
+            /* キーを用いてデータ検索 */
+            for (i = 0; i < keysLen; ++i) {
+                if (removeHeaderKeys[i] == ',') {
+                    removeHeaderKeys[i] = '\0';
+                }
+            }
+            while (headID < keysLen - GET_HEADER_LEN - 1) {
+                dbp = db;
+                while (*dbp) {
+                    if (strcmp(&removeHeaderKeys[headID], *dbp) == 0) {
+                        strcat(data, SPLIT_SYMBOL);
+                        strcat(data, *(++dbp));
+                        headID = strlen(data) + 1;
+                        break;
+                    }
+                    dbp += 2;
+                }
+                if (*dbp == NULL) strcpy(data, "No entry");
+                break;
+            }
+        } else if (strncmp(keys, POST_HEADER, POST_HEADER_LEN) == 0) {
+            strcat(removeHeaderKeys, &keys[GET_HEADER_LEN + 1]);
+            for (i = 0; i < MAX_KEY_LEN; ++i) {
+                if (removeHeaderKeys[i] == ',') {
+                    postTargetName[i] = '\0';
+                    break;
+                }
+                postTargetName[i] = removeHeaderKeys[i];
+            }
+            if (i != MAX_KEY_LEN) {
+                dbp = db;
+                while (*dbp) {
+                    if (strcmp(postTargetName, *dbp) == 0) {
+                        strcpy(data, *(++dbp));
+                        strcpy(*(dbp), &removeHeaderKeys[i + 1]);
+                        break;
+                    }
+                    dbp += 2;
+                }
+                if (*dbp == NULL) strcpy(data, "No entry");
+            }
+        } else
+            strcpy(data, "Bad Header");
+        dataLen = strlen(data);
+        if (sendto(socd, data, dataLen, 0, (struct sockaddr *) &c_address, c_addrlen) != dataLen) {
+            fprintf(stderr, "datagram error\n");
+            exit(1);
+        }
+        printf("Sent data> %s\n", data);
+        memset(data, '\0', sizeof(data) / sizeof(char));
+        memset(removeHeaderKeys, '\0', sizeof(removeHeaderKeys) / sizeof(char));
+    }
 }
