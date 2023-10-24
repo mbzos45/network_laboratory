439b41fca9488c69a8631e0ce6d55b403ff9b40c
mbzos45 <90610406+mbzos45@users.noreply.github.com>
2023-10-24 16:13:39 +0900

    課題2-1完成

--- tcp/vc_client.c
+++ tcp/vc_client.c
@@ -1,25 +1,29 @@
 /* コネクション型の簡単なリモートファイル表示クライアント(vc_client.c) */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h> /* ソケットのための基本的なヘッダファイル      */
 #include <netinet/in.h> /* インタネットドメインのためのヘッダファイル  */
 #include <netdb.h>      /* gethostbyname()を用いるためのヘッダファイル */
-#include <errno.h>
 #include <string.h>
-#define  MAXHOSTNAME    64
-#define     S_TCP_PORT    (u_short)5000
-#define  MAXFILENAME    255
-#define     MAXBUFLEN    512
-#define     ERR        0 /* ファイルオープン失敗 */
-#define  OK        1 /*                 成功 */
+#include <unistd.h>
+#include <stdbool.h>
+
+#define  MAX_HOST_NAME    64
+#define     S_TCP_PORT    7000
+#define  MAX_FILE_NAME    255
+#define     MAX_BUF_LEN    512
+#define CLOSE_HEADER "shutdown:"
+#define PUT_HEADER "PUT:"
+
+const size_t PUT_HEADER_LEN = strlen(PUT_HEADER);
 
 int setup_vcclient(struct hostent *, u_short);
+
 void receive_file(int);
 
-main() {
+int main() {
     int socd;
-    char s_hostname[MAXHOSTNAME];
+    char s_hostname[MAX_HOST_NAME];
     struct hostent *s_hostent;
 
     /* サーバのホスト名の入力 */
@@ -36,15 +40,13 @@ main() {
 
     /* サーバにファイルを要求し受信したファイルの内容を標準出力に出力 */
     receive_file(socd);
-
+    printf("client will close.\n");
     close(socd);
-    exit(0);
+    return 0;
 }
 
 int setup_vcclient(struct hostent *hostent, u_short port) {
     int socd;
-    struct sockaddr_in s_address;
-
     /* インターネットドメインのSOCK_STREAM(TCP)型ソケットの構築 */
     if ((socd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
@@ -52,11 +54,11 @@ int setup_vcclient(struct hostent *hostent, u_short port) {
     }
 
     /* サーバのアドレス(IPアドレスとポート番号)の作成 */
+    struct sockaddr_in s_address;
     bzero((char *) &s_address, sizeof(s_address));
     s_address.sin_family = AF_INET;
     s_address.sin_port = htons(port);
     bcopy((char *) hostent->h_addr, (char *) &s_address.sin_addr, hostent->h_length);
-
     /* サーバとの接続の確立 */
     if (connect(socd, (struct sockaddr *) &s_address, sizeof(s_address)) < 0) {
         perror("connect");
@@ -68,31 +70,50 @@ int setup_vcclient(struct hostent *hostent, u_short port) {
 
 void receive_file(int socd) /* サーバから受け取ったファイルの内容を表示する */
 {
-    char filename[MAXFILENAME + 1];
-    int filename_len;
-    char ack;
-    char buf[MAXBUFLEN];
-    int length;
-
-    /* ファイル名の入力 */
-    printf("remote file name?: ");
-    scanf("%s", filename);
-    /* ファイル名をソケットに書き込む */
-    filename_len = strlen(filename);
-    send(socd, filename, filename_len + 1, 0);
-    /* ファイルオープンに成功したかどうかのメッセージをソケットから読み込む */
-    recv(socd, &ack, 1, 0);
-    switch (ack) {
-        case OK: /* ファイルオープンに成功した場合 */
-            printf("ファイル %s を受信\n", filename);
+    char filename[MAX_FILE_NAME + 1];
+    size_t filename_len;
+    bool ack = false;
+    bool is_shutdown = false;
+    while (1) {
+        /* ファイル名の入力 */
+        printf("remote file name?: ");
+        if (scanf("%s", filename) == EOF) { /* 終了処理時 ファイル名を終了ヘッダにしてサーバーに終了命令を送る*/
+            strcpy(filename, CLOSE_HEADER);
+            is_shutdown = true;
+        }
+        filename_len = strlen(filename);
+        if (strncmp(filename, PUT_HEADER, PUT_HEADER_LEN) == 0) {
+            printf("未実装\n");
+            return;
+        }
+        /* ファイル名をソケットに書き込む */
+        send(socd, filename, filename_len + 1, 0);
+        if (is_shutdown) { /* 終了フラグがtrueの時クライアントを終了する。 */
+            printf("close signal sent to the server.\n");
+            return;
+        };
+        /* ファイルオープンに成功したかどうかのメッセージをソケットから読み込む */
+        recv(socd, &ack, 1, 0);
+        if (ack) {
+            char buf[MAX_BUF_LEN] = {0};
+            printf("received file %s \n", filename);
+            printf("<- start of file ->\n");
             /* ソケットから読み込み標準出力に書き出す */
-            while (length = recv(socd, buf, MAXBUFLEN, 0)) {
-                buf[length] = '\0';
-                fputs(buf, stdout);
+            ssize_t length;
+            while ((length = recv(socd, buf, MAX_BUF_LEN, 0))) {
+                if (buf[length - 1] == EOF) {
+                    buf[0] = '\0';
+                    fputs(buf, stdout);
+                    break;
+                } else {
+                    buf[length] = '\0';
+                    fputs(buf, stdout);
+                }
+
             }
-            break;
-        case ERR: /* ファイルオープンに失敗した場合 */
+            printf("<- end of file ->\n");
+        } else {
             fprintf(stderr, "File access error\n");
-            break;
+        }
     }
 }
--- tcp/vc_server.c
+++ tcp/vc_server.c
@@ -1,29 +1,30 @@
 /* コネクション型の簡単なリモートファイル表示サーバ(vc_server.c) */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/socket.h> /* ソケットのための基本的なヘッダファイル      */
 #include <netinet/in.h> /* インタネットドメインのためのヘッダファイル  */
 #include <netdb.h>      /* gethostbyname()を用いるためのヘッダファイル */
-#include <errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-#define  MAXHOSTNAME    64
-#define  S_TCP_PORT    (u_short)5000  /* 本サーバが用いるポート番号 */
-#define  MAXFILENAME    255
-#define  MAXBUFLEN    512
-#define  ERR        0 /* ファイルオープン失敗 */
-#define     OK        1 /*                 成功 */
+#define MAX_HOST_NAME 64
+#define S_TCP_PORT 7000  /* 本サーバが用いるポート番号 */
+#define MAX_FILE_NAME 255
+#define MAX_BUF_LEN 512
+#define CLOSE_HEADER "shutdown:"
 
 int setup_vcserver(struct hostent *, u_short);
+
 void send_file(int);
 
-main() {
+int main() {
     int socd, socd1;
-    char s_hostname[MAXHOSTNAME];
+    char s_hostname[MAX_HOST_NAME];
     struct hostent *s_hostent;
     struct sockaddr_in c_address;
-    int c_addrlen, cpid;
+    pid_t cpid;
+    socklen_t c_addrlen;
 
     /* サーバのホスト名とそのIPアドレス(をメンバに持つhostent構造体)を求める */
     gethostname(s_hostname, sizeof(s_hostname));
@@ -32,7 +33,7 @@ main() {
     /* バーチャルサーキットサーバの初期設定 */
     socd = setup_vcserver(s_hostent, S_TCP_PORT);
 
-    while (1) {
+    while (true) {
         /* 接続要求の受け入れ */
         c_addrlen = sizeof(c_address);
         if ((socd1 = accept(socd, (struct sockaddr *) &c_address, &c_addrlen)) < 0) {
@@ -43,13 +44,10 @@ main() {
         if ((cpid = fork()) < 0) {
             perror("fork");
             exit(1);
-        }
-        else if (cpid == 0) { /* 子プロセス */
+        } else if (cpid == 0) { /* 子プロセス */
             close(socd);
-
             /* クライアントが要求するファイルの送信 */
             send_file(socd1);
-
             close(socd1);
             exit(0);
         } else close(socd1);   /* 親プロセス */
@@ -58,14 +56,12 @@ main() {
 
 int setup_vcserver(struct hostent *hostent, u_short port) {
     int socd;
-    struct sockaddr_in s_address;
-
     /* インターネットドメインのSOCK_STREAM(TCP)型ソケットの構築 */
     if ((socd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
         exit(1);
     }
-
+    struct sockaddr_in s_address;
     /* アドレス(IPアドレスとポート番号)の作成 */
     bzero((char *) &s_address, sizeof(s_address));
     s_address.sin_family = AF_INET;
@@ -83,33 +79,43 @@ int setup_vcserver(struct hostent *hostent, u_short port) {
         perror("listen");
         exit(1);
     }
-
     return socd;
 }
 
 void send_file(int socd) /* クライアントが要求するファイルを読み込みソケットに書き出す */
 {
-    char filename[MAXFILENAME + 1];
-    FILE *fd;
-    char ack;
-    char buf[MAXBUFLEN];
-
-    /* クライアントから送られるファイル名をソケットから読み込む */
-    recv(socd, filename, MAXFILENAME + 1, 0);
-    /* ファイルを読み出し専用にオープンする */
-    if ((fd = fopen(filename, "r")) != NULL) { /* ファイルオープンに成功した場合 */
-        /* オープン成功メッセージを送る */
-        ack = OK;
-        send(socd, &ack, 1, 0);
-        /* ファイルから1行読み込みソケットに書き出すことをEOFを読むまで繰り返す */
-        printf("ファイル %s を送信\n", filename);
-        while (fgets(buf, MAXBUFLEN, fd)) {
-            send(socd, buf, strlen(buf), 0);
+    while (true) {
+        char recv_str[MAX_FILE_NAME + 1];
+        /* クライアントから送られるファイル名をソケットから読み込む */
+        recv(socd, recv_str, MAX_FILE_NAME + 1, 0);
+        /* 終了命令を受け取った時の処理 */
+        if (strcmp(recv_str, CLOSE_HEADER) == 0) {
+            printf("session close request received\n");
+            break;
         }
-        close(fd);
-    } else {                                    /* ファイルオープンに失敗した場合 */
-        /* オープン失敗メッセージを送る */
-        ack = ERR;
-        send(socd, &ack, 1, 0);
+        /* ファイルを読み出し専用にオープンする */
+        FILE *fd;
+        bool ack;
+        if ((fd = fopen(recv_str, "r")) != NULL) { /* ファイルオープンに成功した場合 */
+            /* オープン成功メッセージを送る */
+            ack = true;
+            send(socd, &ack, 1, 0);
+            /* ファイルから1行読み込みソケットに書き出すことをEOFを読むまで繰り返す */
+            printf("begin sending file %s\n", recv_str);
+            char buf[MAX_BUF_LEN];
+            while (fgets(buf, MAX_BUF_LEN, fd)) {
+                send(socd, buf, strlen(buf), 0);
+            }
+            buf[0] = EOF;
+            buf[1] = '\0';
+            send(socd, buf, strlen(buf), 0); /* EOFを送りファイル送信が終わったことを伝える */
+            printf("sent file %s\n", recv_str);
+            fclose(fd);
+            printf("closed file %s\n", recv_str);
+        } else {                                    /* ファイルオープンに失敗した場合 */
+            /* オープン失敗メッセージを送る */
+            ack = false;
+            send(socd, &ack, 1, 0);
+        }
     }
 }
